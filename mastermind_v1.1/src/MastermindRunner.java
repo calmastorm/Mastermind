47f838ae01f718889543923d15aa7fde5d0fd2d7
calmastorm <94218357+calmastorm@users.noreply.github.com>
2021-12-10 14:06:20 +0800

    Add files via upload

---
+++ mastermind_v1.1/src/MastermindRunner.java
@@ -0,0 +1,10 @@
+
+public class MastermindRunner {
+    public static void main(String[] args){
+        System.out.printf(Toolkit.LAUNCH_MESSAGE);
+        System.out.println("Please enter the number option");
+        MastermindController mm = new MastermindController();
+        mm.Run();
+        System.out.println("Thank you for playing!");
+    }
+}
